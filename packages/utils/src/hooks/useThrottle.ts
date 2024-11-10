28b45c1955349a97f3c1ace77193837e81629ed2
Dg <dgbragas.dev@gmail.com>
2024-11-10 17:42:08 -0300

    feat: add initial 'hooks' functions

---
+++ packages/utils/src/hooks/useThrottle.ts
@@ -0,0 +1,15 @@
+/**
+ * Calculates the area of a rectangle.
+ *
+ * @param {number} width - The width of the rectangle.
+ * @param {number} height - The height of the rectangle.
+ * @returns {number} The area of the rectangle.
+ *
+ * @example
+ * // Example usage:
+ * const sample = useThrottle();
+ * console.log(sample);
+ */
+const useThrottle = () => ({});
+
+export { useThrottle };
