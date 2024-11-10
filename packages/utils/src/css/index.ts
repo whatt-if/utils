27f6ac4cd8ace58b713f75c210d87c64a66b7561
Dg <dgbragas.dev@gmail.com>
2024-11-10 17:41:26 -0300

    feat: add initial 'css' functions

---
+++ packages/utils/src/css/index.ts
@@ -0,0 +1,4 @@
+export * from './compile';
+export * from './makeCn';
+export * from './makeCss';
+export * from './minify';
---
+++ packages/utils/src/css/minify.ts
@@ -0,0 +1,15 @@
+/**
+ * Calculates the area of a rectangle.
+ *
+ * @param {number} width - The width of the rectangle.
+ * @param {number} height - The height of the rectangle.
+ * @returns {number} The area of the rectangle.
+ *
+ * @example
+ * // Example usage:
+ * const sample = minify();
+ * console.log(sample);
+ */
+const minify = () => ({});
+
+export { minify };
