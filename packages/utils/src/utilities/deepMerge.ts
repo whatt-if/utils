15a6b226ada15977ed4dd511e13b602e02fe92e3
Dg <dgbragas.dev@gmail.com>
2024-11-10 17:45:23 -0300

    feat: add initial 'utilities' functions

---
+++ packages/utils/src/utilities/deepMerge.ts
@@ -0,0 +1,15 @@
+/**
+ * Calculates the area of a rectangle.
+ *
+ * @param {number} width - The width of the rectangle.
+ * @param {number} height - The height of the rectangle.
+ * @returns {number} The area of the rectangle.
+ *
+ * @example
+ * // Example usage:
+ * const sample = deepMerge();
+ * console.log(sample);
+ */
+const deepMerge = () => ({});
+
+export { deepMerge };
---
+++ packages/utils/src/utilities/index.ts
@@ -0,0 +1,4 @@
+export * from './copy';
+export * from './debounce';
+export * from './deepMerge';
+export * from './getImageUrl';
