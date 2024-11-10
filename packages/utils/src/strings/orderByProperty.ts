381a966da6986f1e8879502931cb65fd3d81f9cb
Dg <dgbragas.dev@gmail.com>
2024-11-10 17:43:56 -0300

    feat: add initial 'strings' functions

---
+++ packages/utils/src/strings/orderByProperty.ts
@@ -0,0 +1,15 @@
+/**
+ * Calculates the area of a rectangle.
+ *
+ * @param {number} width - The width of the rectangle.
+ * @param {number} height - The height of the rectangle.
+ * @returns {number} The area of the rectangle.
+ *
+ * @example
+ * // Example usage:
+ * const sample = orderByProperty();
+ * console.log(sample);
+ */
+const orderByProperty = () => ({});
+
+export { orderByProperty };
